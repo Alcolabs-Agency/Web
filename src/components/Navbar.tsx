5429f4a6d47f2b8dcc766f16f05f55b4f86895ee
Yesenia <aryesigon@gmail.com>
2024-10-08 17:57:51 -0300

    Update Navbar and SocialMedia components, add pages.tsx, and remove page.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
 "use client";
 import Image from "next/legacy/image";
 import React from "react";
 import Link from "next/link";
 import { useEffect , useState} from "react";
 
-const Navbar = () => {
+const MiNavbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   useEffect(() => {
     //algo
@@ -62,37 +61,35 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
-=======
-"use client";
-import Image from "next/legacy/image";
-import React from "react";
-import Link from "next/link";
-import { useEffect } from "react";
+export default MiNavbar;
+// "use client";
+// import Image from "next/legacy/image";
+// import React from "react";
+// import Link from "next/link";
+// import { useEffect } from "react";
 
-const Navbar = () => {
-  useEffect(() => {
-    //algo
-  }, []);
-  return (
-    <nav className="flex items-center px-16 py-6 absolute top-3 left-14 right-0 z-10 bg-transparent">
-      <div className="flex items-center space-x-8">
-        <Image
-          src="/images/Logo horizontal2.png"
-          alt="Alcolab Logo"
-          width={150}
-          height={50}
-        />
-        <div className="flex space-x-10  text-white font-bold text-xl">
+// const Navbar = () => {
+//   useEffect(() => {
+//     //algo
+//   }, []);
+//   return (
+//     <nav className="flex items-center px-16 py-6 absolute top-3 left-14 right-0 z-10 bg-transparent">
+//       <div className="flex items-center space-x-8">
+//         <Image
+//           src="/images/Logo horizontal2.png"
+//           alt="Alcolab Logo"
+//           width={150}
+//           height={50}
+//         />
+//         <div className="flex space-x-10  text-white font-bold text-xl">
           
-          <Link href="#about">About Us</Link>
-          <Link href="#services">Services</Link>
-          <Link href="#solution">Solution</Link>
-        </div>
-      </div>
-    </nav>
-  );
-};
+//           <Link href="#about">About Us</Link>
+//           <Link href="#services">Services</Link>
+//           <Link href="#solution">Solution</Link>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// };
 
-export default Navbar;
->>>>>>> Yesenia
+// export default Navbar;
