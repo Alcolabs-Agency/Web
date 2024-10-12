488e14fc2be35a78fe4601e78d39900382275571
Yesenia <aryesigon@gmail.com>
2024-10-12 20:27:04 -0300

    reduccion de espacios

--- src/components/Analytics.js
+++ src/components/Analytics.js
@@ -11,9 +11,9 @@ const Analytics = () => {
         <ScrollTrigger onEnter={() => setCounter(true)} onExit={() => setCounter(false)}>
 
             <section className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-[#3D0079] to-black"> 
-            <div className="text-white text-center mb-16">
-                <h1 className="text-5xl font-bold --font-sf-pro mb-4">Analytics</h1>
-                <p className="text-base --font-sf-pro mx-4 md:mx-8">Assisting brands in painting their digital canvases with our technology-infused solutions. Emphasizing on the stats apart from the words.</p>
+            <div className="text-white text-center mb-40">
+                <h1 className="text-5xl font-bold --font-sf-pro mb-28">Analytics</h1>
+                <p className="text-base --font-sf-pro mx-4 md:mx-8 -mb-12">Assisting brands in painting their digital canvases with our technology-infused solutions. Emphasizing on the stats apart from the words.</p>
             </div>
             <div className="flex flex-col md:flex-row justify-center mx-4 md:space-x-20 text-white">
                 <div className="flex flex-col items-center">
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer
       className="py-8 text-white"
       style={{
-        background: 'linear-gradient(90deg, rgba(4,4,4,1) 75%, rgba(32,15,45,1) 97%)',
+        background: 'linear-gradient(90deg, rgba(4,4,4,1) -40%, rgba(32,15,45,0) 25%)',
       }}
     >
       <div className=" max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 px-4">
@@ -26,7 +26,7 @@ const Footer = () => {
 
         <div className="flex justify-center md:justify-end space-x-20">
         <div className="flex flex-col items-center md:items-start">
-          <h3 className="text-lg font-semibold text-purple-950 mb-4">Home</h3>
+          <h3 className="text-lg font-bold text-white mb-4">Home</h3>
           <ul className="text-center md:text-left">
             <li className="mb-2">
               <a href="#" className="text-white hover:text-purple-200">About Us</a>
@@ -41,10 +41,10 @@ const Footer = () => {
         </div>
 
           <div>
-            <h3 className="text-lg font-semibold text-purple-950 mb-4 font-SFmedium">Follow Us</h3>
+            <h3 className="text-lg font-bold text-white mb-4 font-SFmedium">Follow Us</h3>
             <ul className="flex flex-col items-start space-y-2">
               <li className="flex items-center space-x-2">
-                <FaLinkedinIn className="text-white hover:text-purple-200" />
+                <FaLinkedinIn className="text-white hover:text-gray-100" />
                 <a href="https://www.linkedin.com/company/alcolab" className="text-white hover:text-purple-200 font-SFmedium">LinkedIn</a>
               </li>
               <li className="flex items-center space-x-2">
@@ -60,11 +60,11 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="flex justify-around pt-20 mt-8 border-gray-700  text-gray-400 text-center">
-        <p className="text-gray-500 ">Copyright © 2024</p>
+      <div className="flex justify-around pt-20 mt-8 border-white  text-white text-center">
+        <p className="text-white ">Copyright © 2024</p>
         <span>
-        <a href="#" className="hover:text-purple-200 mx-5">Terms & Conditions</a>
-        <a href="#" className="hover:text-purple-200">Privacy Policy</a>
+        <a href="#" className="hover:text-gray-100 mx-5">Terms & Conditions</a>
+        <a href="#" className="hover:text-gray-100">Privacy Policy</a>
       </span>
     </div>
   </footer>
