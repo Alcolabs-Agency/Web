a048700894a12210b5a406ff5cc2579f4607e3af
Yeseviola <aryesigon@gmail.com>
2025-01-23 18:01:44 -0300

    cambio de forma de simplificar la directiva una sola linea de connect.src para que interprete correcto y se pueda incluir todo los dominios permitidos localhost y render

--- next.config.mjs
+++ next.config.mjs
@@ -7,21 +7,27 @@ const nextConfig = {
         headers: [
           {
             key: "Content-Security-Policy",
-            value: `
-              default-src 'self';
-              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com https://www.gstatic.com https://cdn.jsdelivr.net;
-              worker-src 'self' blob:;
-              connect-src 'self' http://127.0.0.1:5000 http://localhost:5000 blob: data: https://cdn.jsdelivr.net https://backend-ocr.onrender.com;
-              img-src 'self' https://www.gstatic.com data: blob:;
-              frame-src 'self' https://calendar.google.com;
-              style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-              font-src 'self' https://fonts.gstatic.com;
-            `.replace(/\n/g, ""),
-          },
-        ],
-      },
+            value: 
+            "default-src 'self'; " +
+            // Scripts
+            "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com https://www.gstatic.com https://cdn.jsdelivr.net; " +
+            // Workers
+            "worker-src 'self' blob:; " +
+            // Aquí todos tus dominios de fetch
+            "connect-src 'self' http://127.0.0.1:5000 http://localhost:5000 blob: data: https://cdn.jsdelivr.net https://backend-ocr.onrender.com; " +
+            // Imágenes
+            "img-src 'self' https://www.gstatic.com data: blob:; " +
+            // Frames
+            "frame-src 'self' https://calendar.google.com; " +
+            // Estilos
+            "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
+            // Fuentes
+            "font-src 'self' https://fonts.gstatic.com;"
+          }
+        ]
+      }
     ];
-  },
+  }
 };
 
 export default nextConfig;
