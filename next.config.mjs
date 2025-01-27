76f4281e5a0c2966a3068c246548d5356f1975ca
Yeseviola <aryesigon@gmail.com>
2025-01-27 16:21:41 -0300

    reestructurando url ngrok dinamica

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,7 @@ const nextConfig = {
               "default-src 'self'; " +
               "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com https://www.gstatic.com https://cdn.jsdelivr.net; " +
               "worker-src 'self' blob:; " +
-              "connect-src 'self' blob: data: https://cdn.jsdelivr.net https://backend-ocr.onrender.com https://f68e-2802-8010-d540-9700-ae68-8b00.ngrok-free.app https://*.ngrok-free.app; " +
+              "connect-src 'self' blob: data: https://cdn.jsdelivr.net https://backend-ocr.onrender.com https://*.ngrok-free.app; " +
               "img-src 'self' https://www.gstatic.com data: blob:; " +
               "frame-src 'self' https://calendar.google.com; " +
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
